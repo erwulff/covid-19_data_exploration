c03173f6cfc4522360dfc048e07c998fd3c1fe2a
erwulff <eric.g.t.wulff@gmail.com>
2020-04-04 18:42:39 +0200

    feat: add server

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # dash app starts here
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div([
 
     dcc.Markdown('''
